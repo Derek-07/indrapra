bd312290b364165d2977ae5a14942d3d95ce6d58
derekart <aaruesportsathlete@gmail.com>
2026-07-10 16:22:09 +0530

    Show fallback images in admin portal grids

---
+++ backend/db_check.js
@@ -0,0 +1,9 @@
+﻿const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://dgo733513:30aWkU2O2Nn0vSgE@indra.27581.mongodb.net/?retryWrites=true&w=majority&appName=indra');
+const Project = mongoose.model('Project', new mongoose.Schema({ title: String, imageUrl: String }, { strict: false }));
+async function run() {
+    const projs = await Project.find({}, 'title imageUrl');
+    console.log(projs);
+    process.exit();
+}
+run();
